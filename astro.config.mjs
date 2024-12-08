31fc92377a4bf5cdbbb0674aacfae9abfc5075a1
Alejandro Llanganate <alejandro.llanganate@owasp.org>
2024-12-08 07:40:08 -0500

    feat: add new changes ui

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
-import { defineConfig } from 'astro/config';
-
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import icon from "astro-icon";
 
-// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), mdx(), sitemap(), icon()],
   site: 'https://www.alejandrollanganate.com',
 });
---
+++ src/content/team/alejandro-llanganate.md
@@ -0,0 +1,10 @@
+---
+draft: false
+name: "Alejandro Llanganate"
+title: "Software Engineer 💻 & Entrepreneur"
+avatar: {
+    src: "https://i.ibb.co/M8HVb7L/Whats-App-Image-2024-12-07-at-23-27-28.jpg",
+    alt: "Marcell Ziemann"
+}
+publishDate: "2022-11-08 15:39"
+---
